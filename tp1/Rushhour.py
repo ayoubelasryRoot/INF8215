2afad20375991263cbc359de9c8ba9a914435ea6
Ayoub El Asry <ayoub.elasry96@gmail.com>
2019-10-05 16:16:56 -0400

    finish first aprt of the lab

--- tp1/Rushhour.py
+++ tp1/Rushhour.py
@@ -11,7 +11,7 @@ class Rushhour:
         self.length = length
         self.move_on = move_on
         self.color = color
-        
+        #matrix
         self.free_pos = None
     
     def init_positions(self, state: State):
@@ -24,18 +24,67 @@ class Rushhour:
             else:
                 self.free_pos[y: y + self.length[i], x] = False
     
-    def possible_moves(self, state):
+    def possible_moves(self, state: State):
         self.init_positions(state)
-        new_states = []
-        # TODO
-        return new_states
+        pos_states = []
+        # each car we can move is a possible state
+        for i in range(self.nbcars):
+            x = self.move_on[i]
+            y = state.pos[i]         
+            if self.horiz[i]:
+                # see left part
+                k = y - 1
+                current_state = state
+                while k >= 0:
+                    if self.free_pos[x][k] == True:
+                        current_state = self.move_left_or_up(i, current_state)
+                        pos_states.append(current_state)
+                    else:
+                        break
+                    k -= 1
+                # see rigth part
+                k = y + self.length[i]
+                current_state = state 
+                while k < 6:
+                    if self.free_pos[x][k] == True:
+                        current_state = self.move_rigth_or_down(i, current_state)
+                        pos_states.append(current_state)
+                    else:
+                        break
+                    k += 1
+            else:
+                # see left part
+                k = y - 1
+                current_state = state
+                while k >= 0:
+                    if self.free_pos[k][x] == True:
+                        current_state = self.move_left_or_up(i, current_state)
+                        pos_states.append(current_state)
+                    else:
+                        break
+                    k -= 1
+                # see rigth part
+                k = y + self.length[i]
+                current_state = state 
+                while k < 6:
+                    if self.free_pos[k][x] == True:
+                        current_state = self.move_rigth_or_down(i, current_state)
+                        pos_states.append(current_state)
+                    else:
+                        break
+                    k += 1
+
+        return pos_states
     
-    def solve(self, state):
-        visited = set()
-        fifo = deque([state])
-        visited.add(state)
-        # TODO
-        
+
+    def move_left_or_up(self, car_id: int, state: State):
+        return state.move(car_id, -1)
+    
+    def move_rigth_or_down(self, car_id: int, state: State):
+        return state.move(car_id, 1)
+
+
+    def solve(self, state: State):
         return None
     
                     
