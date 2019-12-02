c2470646da8ca66f66aedceedcc894e7959dff72
Ayoub El Asry <ayoub.elasry96@gmail.com>
2019-12-02 14:36:13 -0500

    tp 2

--- tp2/code/main.py
+++ tp2/code/main.py
@@ -26,15 +26,15 @@ def test_print_move():
 # algo.solve(s, True)
 
 
-# rh = Rushhour([True, False, False, False, True],
-#                  [2, 3, 2, 3, 3],
-#                  [2, 4, 5, 1, 5],
-#                  ["rouge", "vert", "bleu", "orange", "jaune"])
-# s = State([1, 0, 1, 3, 2])
-# algo = MiniMaxSearch(rh, s, 1) 
-# algo.rushhour.init_positions(s)
-# print(algo.rushhour.free_pos)
-# algo.solve(s, True)
+rh = Rushhour([True, False, False, False, True],
+                 [2, 3, 2, 3, 3],
+                 [2, 4, 5, 1, 5],
+                 ["rouge", "vert", "bleu", "orange", "jaune"])
+s = State([1, 0, 1, 3, 2])
+algo = MiniMaxSearch(rh, s, 1) 
+algo.rushhour.init_positions(s)
+print(algo.rushhour.free_pos)
+algo.solve(s, True)
 
 
 # # solution optimale: 16 moves
@@ -48,12 +48,12 @@ def test_print_move():
 # print(algo.rushhour.free_pos)
 # algo.solve(s, True) 
 
-rh = Rushhour([True, False, True, False, False, False, True, True, False, True, True],
-                 [2, 2, 3, 2, 2, 3, 3, 2, 2, 2, 2],
-                 [2, 0, 0, 3, 4, 5, 3, 5, 2, 5, 4],
-                 ["rouge", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
-s = State([0, 0, 3, 1, 2, 1, 0, 0, 4, 3, 4])
-algo = MiniMaxSearch(rh, s,1)
-algo.rushhour.init_positions(s)
-print(algo.rushhour.free_pos)
-algo.solve(s, True)
+# rh = Rushhour([True, False, True, False, False, False, True, True, False, True, True],
+#                  [2, 2, 3, 2, 2, 3, 3, 2, 2, 2, 2],
+#                  [2, 0, 0, 3, 4, 5, 3, 5, 2, 5, 4],
+#                  ["rouge", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+# s = State([0, 0, 3, 1, 2, 1, 0, 0, 4, 3, 4])
+# algo = MiniMaxSearch(rh, s,1)
+# algo.rushhour.init_positions(s)
+# print(algo.rushhour.free_pos)
+# algo.solve(s, True)
